3c82fe97f45b60aba64c54f322e7ae3348d29918
JonathanLuna95 <jjlunap95@gmail.com>
2026-08-16 23:25:55 -0500

    Crear clase Banco

---
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -0,0 +1,37 @@
+package com.krakedev.financiero.servicios;
+
+import com.krakedev.financiero.entidades.Cliente;
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class Banco {
+	
+	private int ultimoCodigo = 1000;
+	
+	public Banco() {
+
+	}
+
+	public int getUltimoCodigo() {
+		return ultimoCodigo;
+	}
+
+	public void setUltimoCodigo(int ultimoCodigo) {
+		this.ultimoCodigo = ultimoCodigo;
+	}
+	
+	public Cuenta crearCuenta(Cliente cliente) {
+
+		String codigoStr = ultimoCodigo + "";
+
+		ultimoCodigo = ultimoCodigo + 1;
+
+		Cuenta cuenta = new Cuenta(codigoStr);
+
+		cuenta.setPropietario(cliente);
+
+		return cuenta;
+	}
+	
+	
+
+}
